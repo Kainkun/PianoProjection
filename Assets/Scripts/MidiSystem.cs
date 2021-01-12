e46287e7b252ce14802b215237f8c7fddc2a400f
Kain Shepard <kainkun100@gmail.com>
2021-01-11 17:51:17 -0800

    using DryWetMidi for midi features. got playback working

---
+++ Assets/Scripts/MidiSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
+
+public class MidiSystem : MonoBehaviour
+{
+    MidiFile midiFile;
+    OutputDevice outputDevice;
+    Playback playback;
+
+    void Start()
+    {
+        // print(OutputDevice.GetDevicesCount());
+        // foreach (var item in OutputDevice.GetAll())
+        // {
+        //     print(item.Name);
+        // }
+
+        midiFile = MidiFile.Read("Assets/Midi/Clair.mid");
+        outputDevice = OutputDevice.GetByName("VirtualMIDISynth #1");
+        playback = midiFile.GetPlayback(outputDevice, new MidiClockSettings());
+
+        StartCoroutine(Play());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Play()
+    {
+        playback.Start();
+        while (playback.IsRunning)
+        {
+            playback.TickClock();
+            yield return null;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        playback.Stop();
+        playback.Dispose();
+    }
+}
